547de95c699a26e24ed0be507f66b95e12788b96
kelly25 <bingjiez@usc.edu>
2016-04-23 17:49:04 -0700

    1. for coreNER, before traverse folder, it works for a file 2. for OpenNLP, it works for folders, get 7 class name entities

--- CoreNER.java
+++ CoreNER.java
@@ -10,11 +10,35 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import edu.stanford.nlp.util.Triple;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class CoreNER {
+    private static ArrayList<String> filelist = new ArrayList<>();
+    //Set<String> person_set=new HashSet<>();
+    static Set<String> person = new HashSet<>();
+    static Set<String> organization = new HashSet<>();
+    static Set<String> location = new HashSet<>();
+    static Set<String> date = new HashSet<>();
+    static Set<String> time = new HashSet<>();
+    static Set<String> percent = new HashSet<>();
+    static Set<String> money = new HashSet<>();
+
+    static void getFiles(String filepath) {
+        File root = new File(filepath);
+        File[] files = root.listFiles();
+        for (File file : files) {
+            if (file.getName().equals(".DS_Store"))
+                continue;
+            if (file.isDirectory())
+                getFiles(file.getAbsolutePath());
+            filelist.add(file.getAbsolutePath());
+        }
+
+    }
     public static void main(String[] args) throws Exception {
 
         String serializedClassifier = "/Users/Kelly/Downloads/stanford-ner-2015-12-09/classifiers/english.muc.7class.distsim.crf.ser.gz";
@@ -34,15 +58,10 @@ public class CoreNER {
          to get the entities in the String with character offsets, and
          (3) how to run NER on a whole file (without loading it into a String).
       */
-        Set<String> person = new HashSet<>();
-        Set<String> organization = new HashSet<>();
-        Set<String> location = new HashSet<>();
-        Set<String> date = new HashSet<>();
-        Set<String> time = new HashSet<>();
-        Set<String> percent = new HashSet<>();
-        Set<String> money = new HashSet<>();
         String path = "/Users/Kelly/Documents/test.txt";
         String fileContents = IOUtils.slurpFile(path);
+        getFiles("/Users/Kelly/Documents/rawdata/kelly/application_xml/");
+
         List<Triple<String, Integer, Integer>> list = classifier.classifyToCharacterOffsets(fileContents);
         for (Triple<String, Integer, Integer> item : list) {
             System.out.println(item.first() + ": " + fileContents.substring(item.second(), item.third()));
--- OpenNLP.java
+++ OpenNLP.java
@@ -28,77 +28,70 @@ public class OpenNLP {
     static Set<String> percent = new HashSet<>();
     static Set<String> money = new HashSet<>();
     private static String type;
+
     static void getFiles(String filepath) {
         File root = new File(filepath);
         File[] files = root.listFiles();
         for (File file : files) {
             if (file.getName().equals(".DS_Store"))
                 continue;
-            if(file.isDirectory())
+            if (file.isDirectory())
                 getFiles(file.getAbsolutePath());
             filelist.add(file.getAbsolutePath());
         }
 
     }
-    private static String readFile(String filepath)
-    {
-        StringBuilder sb=new StringBuilder();
-        File file=new File(filepath);
-        BufferedReader bf=null;
-        try{
-            bf=new BufferedReader(new FileReader(file));
-            String data=null;
-            while((data=bf.readLine())!=null)
+
+    private static String readFile(String filepath) {
+        StringBuilder sb = new StringBuilder();
+        File file = new File(filepath);
+        BufferedReader bf = null;
+        try {
+            bf = new BufferedReader(new FileReader(file));
+            String data = null;
+            while ((data = bf.readLine()) != null)
                 sb.append(data);
 
-        }catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 bf.close();
-            }catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
         return sb.toString();
     }
-    private static String getText(String filepath)
-    {
-        Tika tika=new Tika();
-        String text="";
-        try{
+
+    private static String getText(String filepath) {
+        Tika tika = new Tika();
+        String text = "";
+        try {
             //get text content
-            text=tika.parseToString(new File(filepath));
-        }catch (Exception e)
-        {
-            System.out.println("error filepath is :"+filepath);
+            text = tika.parseToString(new File(filepath));
+        } catch (Exception e) {
+            System.out.println("error filepath is :" + filepath);
             e.printStackTrace();
         }
         return text;
     }
-    private static String[] getSentences(String content)throws Exception
-    {
-        InputStream modelInSen=new FileInputStream("/Users/Kelly/Downloads/en-sent.bin");
-        String sentences[]=null;
-        try{
-            SentenceModel model_sen=new SentenceModel(modelInSen);
-            SentenceDetectorME sentenceDetector=new SentenceDetectorME(model_sen);
-            sentences=sentenceDetector.sentDetect(content);
-        }catch (Exception e)
-        {
+
+    private static String[] getSentences(String content) throws Exception {
+        InputStream modelInSen = new FileInputStream("/Users/Kelly/Downloads/en-sent.bin");
+        String sentences[] = null;
+        try {
+            SentenceModel model_sen = new SentenceModel(modelInSen);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(model_sen);
+            sentences = sentenceDetector.sentDetect(content);
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(modelInSen!=null)
-            {
-                try{
+        } finally {
+            if (modelInSen != null) {
+                try {
                     modelInSen.close();
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -106,30 +99,24 @@ public class OpenNLP {
         }
         return sentences;
     }
-    private static String[][] getTokens(String[] sentences)throws Exception
-    {
-        InputStream modelInToken=new FileInputStream("/Users/Kelly/Downloads/en-token.bin");
-        String tokens[][]=new String[sentences.length][];
-        try{
-            TokenizerModel model_token=new TokenizerModel(modelInToken);
-            Tokenizer tokenizer=new TokenizerME(model_token);
-            for(int i=0;i< sentences.length;i++)
-            {
-                tokens[i]=tokenizer.tokenize(sentences[i]);
+
+    private static String[][] getTokens(String[] sentences) throws Exception {
+        InputStream modelInToken = new FileInputStream("/Users/Kelly/Downloads/en-token.bin");
+        String tokens[][] = new String[sentences.length][];
+        try {
+            TokenizerModel model_token = new TokenizerModel(modelInToken);
+            Tokenizer tokenizer = new TokenizerME(model_token);
+            for (int i = 0; i < sentences.length; i++) {
+                tokens[i] = tokenizer.tokenize(sentences[i]);
             }
 
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(modelInToken!=null)
-            {
-                try{
+        } finally {
+            if (modelInToken != null) {
+                try {
                     modelInToken.close();
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -137,67 +124,62 @@ public class OpenNLP {
         }
         return tokens;
     }
-    private static void getNameEntities(String[][] tokens,String modelpath,String model_type)throws  Exception
-    {
+
+    private static void getNameEntities(String[][] tokens, String modelpath, String model_type) throws Exception {
         //String type="";
-        InputStream modelIn=new FileInputStream(modelpath);
-        try{
-            TokenNameFinderModel model=new TokenNameFinderModel(modelIn);
-            NameFinderME nameFinder=new NameFinderME(model);
-            Span nameSpans[]=null;
-            for(int i=0;i< tokens.length;i++)
-            {
-                nameSpans=nameFinder.find(tokens[i]);
-                if(nameSpans==null || nameSpans.length==0) continue;
-                int start=nameSpans[0].getStart();
-                int end=nameSpans[0].getEnd();
-                for(int j=start;j<end;j++)
-                {
-                    /*switch (model_type)
-                    {
-                        case "PERSON":
-                            person.add(tokens[i][j]);
-                            break;
-                        case "ORGANIZATION":
-                            organization.add(tokens[i][j]);
-                            break;
-                        case "LOCATION":
-                            location.add(tokens[i][j]);
-                            break;
-                        case "MONEY":
-                            money.add(tokens[i][j]);
-                            break;
-                        case "PERCENT":
-                            percent.add(tokens[i][j]);
-                            break;
-                        case "DATE":
-                            date.add(tokens[i][j]);
-                            break;
-                        case "TIME":
-                            time.add(tokens[i][j]);
-                            break;
-                    }*/
-                    System.out.println(tokens[i][j]);
+        InputStream modelIn = new FileInputStream(modelpath);
+        try {
+            TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+            NameFinderME nameFinder = new NameFinderME(model);
+            Span nameSpans[] = null;
+            for (int i = 0; i < tokens.length; i++) {
+                nameSpans = nameFinder.find(tokens[i]);
+                if (nameSpans == null || nameSpans.length == 0) continue;
+                int start = nameSpans[0].getStart();
+                int end = nameSpans[0].getEnd();
+                StringBuilder sb = new StringBuilder();
+                for (int j = start; j < end; j++) {
+                    sb.append(tokens[i][j] + " ");
+                    //System.out.println(tokens[i][j]);
                 }
-                    //res.add(tokens[i][j]);
+                switch (model_type) {
+                    case "PERSON":
+                        person.add(sb.toString());
+                        break;
+                    case "ORGANIZATION":
+                        organization.add(sb.toString());
+                        break;
+                    case "LOCATION":
+                        location.add(sb.toString());
+                        break;
+                    case "MONEY":
+                        money.add(sb.toString());
+                        break;
+                    case "PERCENT":
+                        percent.add(sb.toString());
+                        break;
+                    case "DATE":
+                        date.add(sb.toString());
+                        break;
+                    case "TIME":
+                        time.add(sb.toString());
+                        break;
+                }
+                System.out.println(sb.toString());
+                //res.add(tokens[i][j]);
             }
-            if(nameSpans!=null && nameSpans.length!=0)
-                type=nameSpans[0].getType();
+            if (nameSpans != null && nameSpans.length != 0)
+                type = nameSpans[0].getType();
 
             //res.add(type);
 
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(modelIn!=null)
-            {
-                try{
+        } finally {
+            if (modelIn != null) {
+                try {
                     modelIn.close();
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -205,45 +187,37 @@ public class OpenNLP {
         }
         //return res;
     }
-    public static void main(String args[])throws Exception
-    {
+
+    public static void main(String args[]) throws Exception {
         //String text=getText("/Users/Kelly/Documents/rawdata/kelly/application_atom+xml/09D447735AD3E4FB27E7C1646328DFF481D1220512B99B10AD9C4A5C7152E903");
-        String filepath="/Users/Kelly/Documents/test.txt";
-        String person_model="/Users/Kelly/Downloads/en-ner-person.bin";
-        String date_model="/Users/Kelly/Downloads/en-ner-date.bin";
-        String location_model="/Users/Kelly/Downloads/en-ner-location.bin";
-        String money_model="/Users/Kelly/Downloads/en-ner-money.bin";
-        String organization_model="/Users/Kelly/Downloads/en-ner-organization.bin";
-        String percentage_model="/Users/Kelly/Downloads/en-ner-percentage.bin";
-        String time_model="/Users/Kelly/Downloads/en-ner-time.bin";
-        getFiles("/Users/Kelly/Documents/rawdata/kelly/");
+        //String filepath="/Users/Kelly/Documents/test2.txt";
+        String person_model = "/Users/Kelly/Downloads/en-ner-person.bin";
+        String date_model = "/Users/Kelly/Downloads/en-ner-date.bin";
+        String location_model = "/Users/Kelly/Downloads/en-ner-location.bin";
+        String money_model = "/Users/Kelly/Downloads/en-ner-money.bin";
+        String organization_model = "/Users/Kelly/Downloads/en-ner-organization.bin";
+        String percentage_model = "/Users/Kelly/Downloads/en-ner-percentage.bin";
+        String time_model = "/Users/Kelly/Downloads/en-ner-time.bin";
+        getFiles("/Users/Kelly/Documents/rawdata/kelly/application_xml/");
         //List< List<String>> res=new ArrayList<>();
-        String content=getText(filepath);
-        if(content==null || content.length()==0)
-            return;
-        String[] sentences=getSentences(content);
-        if(sentences==null || sentences.length==0)
-            return;
-        String[][] tokens=getTokens(sentences);
-        if(tokens==null ||tokens.length==0)
-            return;
-        getNameEntities(tokens, person_model,"PERSON");
-       // getNameEntities(tokens, organization_model);
-       // getNameEntities(tokens, location_model);
-        System.out.println("hello");
-        /*for(int i=0;i<filelist.size();i++)
-        {
+        for (int i = 0; i < filelist.size(); i++) {
             //String content=readFile(filelist.get(i));
-            String content=getText(filelist.get(i));
-            if(content==null || content.length()==0)
+            String content = getText(filelist.get(i));
+            if (content == null || content.length() == 0)
                 continue;
-            String[] sentences=getSentences(content);
-            if(sentences==null || sentences.length==0)
+            String[] sentences = getSentences(content);
+            if (sentences == null || sentences.length == 0)
                 continue;
-            String[][] tokens=getTokens(sentences);
-            if(tokens==null ||tokens.length==0)
+            String[][] tokens = getTokens(sentences);
+            if (tokens == null || tokens.length == 0)
                 continue;
-            getNameEntities(tokens,person_model);
+            getNameEntities(tokens, person_model, "PERSON");
+            getNameEntities(tokens, date_model, "DATE");
+            getNameEntities(tokens, location_model, "LOCATION");
+            getNameEntities(tokens, money_model, "MONEY");
+            getNameEntities(tokens, organization_model, "ORGANIZATION");
+            getNameEntities(tokens, percentage_model, "PERCENTAGE");
+            getNameEntities(tokens, time_model, "TIME");
            /* for(int j=0;j<tmp.size();j++)
             {
                 person_set.add(tmp.get(j));
@@ -251,7 +225,7 @@ public class OpenNLP {
             //res.add(tmp);
             //person_set.add()
         }
-        //System.out.println("Elements are :");
+        System.out.println("---------------------------------------------------------------------------------");
         /*InputStream modelIn=new FileInputStream("/Users/Kelly/Downloads/en-ner-person.bin");
         InputStream modelInSen=new FileInputStream("/Users/Kelly/Downloads/en-sent.bin");
         InputStream modelInToken=new FileInputStream("/Users/Kelly/Downloads/en-token.bin");
@@ -296,3 +270,4 @@ public class OpenNLP {
             }
         }*/
     }
+}
